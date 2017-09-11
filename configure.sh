5ad38db82f00702f52c62126c6b8f19823ee4773
harisvsulaiman <harisvsulaiman@gmail.com>
2017-09-12 04:33:59 +0530

    try debian based js binary build

--- configure.sh
+++ configure.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Installing nodejs"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get update
 sudo apt-get install -y nodejs && sudo apt-get install -y build-essential
+npm install
+npm install -g pkg
+npm run build
