8c306b6c87086ddc25f625222d48a104026ecbe9
harisvsulaiman <harisvsulaiman@gmail.com>
2017-09-12 22:37:33 +0530

    new method

--- configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# echo "Installing nodejs"
-# npm install
-# npm install -g pkg
-# npm run build
---
+++ nodejs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing nodejs"
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+. ~/.bashrc
+mkdir ~/local
+mkdir ~/node-latest-install
+cd ~/node-latest-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=$HOME/local
+make install
+curl -L https://www.npmjs.com/install.sh | sh
