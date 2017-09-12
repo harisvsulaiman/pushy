de8ab568e40b051091dc344f80aec2f98566698b
harisvsulaiman <harisvsulaiman@gmail.com>
2017-09-12 11:42:07 +0530

    removed sudo from configure bash

--- configure.sh
+++ configure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Installing nodejs"
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs && sudo apt-get install -y build-essential
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup_8.x | -E bash -
+apt-get install -y nodejs && apt-get install -y build-essential
 npm install
 npm install -g pkg
 npm run build
