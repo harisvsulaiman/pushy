b76fb63dfc5b8e5a9a8eae3a05bea4a5fff9c078
harisvsulaiman <harisvsulaiman@gmail.com>
2017-09-12 12:01:43 +0530

    control file

--- configure.sh
+++ configure.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo "Installing nodejs"
-apt-get install curl
-curl -sL https://deb.nodesource.com/setup_8.x | -E bash -
-apt-get install -y nodejs && apt-get install -y build-essential
 npm install
 npm install -g pkg
 npm run build
