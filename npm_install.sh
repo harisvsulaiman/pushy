e72d06db7d84b96b5c54cd55d8ddef6848aedc7b
harisvsulaiman <harisvsulaiman@gmail.com>
2017-09-16 11:54:50 +0530

    Build app entirely on meson and pkg

--- npm_install.sh
+++ npm_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-npm install -g pkg
-npm run build
+# Install pkg
+npm install -g zeit/pkg#aa6e7a490c0f6c9e21abad93a384014db3331806
+
+# Install and build app.
+npm install && npm run build
