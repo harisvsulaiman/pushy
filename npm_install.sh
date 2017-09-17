035c3a2104261135b5e8e96172a36e0ca3217ef7
harisvsulaiman <harisvsulaiman@gmail.com>
2017-09-18 00:14:15 +0530

    npm log file added

--- npm_install.sh
+++ npm_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 NODE_DIR="com.github.harisvsulaiman.pushy.node"
 
 if [ -d ${NODE_DIR} ]
 then
+    echo "Deleting Node directory"
     rm -rf ${NODE_DIR}
 fi
 
@@ -13,15 +15,16 @@ then
     npm install
 fi
 
-echo "Copying binaries"
 mkdir ${NODE_DIR}
 
 # Build app using package
 echo "Building binary using pkg"
-npm run build
+npm run build &> npm_log.txt
 
 cp ./node_modules/keytar/build/Release/keytar.node ${NODE_DIR}
 cp ./node_modules/abstract-socket/build/Release/abstract_socket.node ${NODE_DIR}
 cp ./node_modules/websocket/build/Release/bufferutil.node ${NODE_DIR}
 cp ./node_modules/websocket/build/Release/validation.node ${NODE_DIR}
 cp ./node_modules/opn/xdg-open ${NODE_DIR}
+
+exit 0;
