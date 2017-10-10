4e5f19f61c04620f5c1aef33aade96e3239e619d
harisvsulaiman <harisvsulaiman@gmail.com>
2017-10-11 05:07:21 +0530

    sentry key change

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const RAVEN_URL = "https://eee3dc743c7a43b898955307deab54cc@sentry.io/213857";
+const RAVEN_URL = "https://eee3dc743c7a43b898955307deab54cc:4183890553ba40b4ae491b8e2d32d052@sentry.io/213857";
 module.exports = {
   RAVEN_URL
 };
